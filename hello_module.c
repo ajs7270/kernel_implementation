f89052e9abad736cef4f3bd7be498d9ce99954b5
jisuAn <ajs7270@naver.com>
2019-07-25 00:03:17 +0900

    fix kernel module

--- hello_module.c
+++ hello_module.c
@@ -2,18 +2,18 @@
 #include <linux/module.h>
 #include <linux/init.h>
 
-static int __init hello_module_init(void)
+int __init hello_module_init(void)
 {
 	printk(KERN_EMERG "Hello Module~! I'm jisu \n");
 	return 0;
 }
 
-static void __exit hello_module_cleanup(void)
+void __exit hello_module_cleanup(void)
 {
 	printk("<0>Bye Module~!\n");
 }
 
 module_init(hello_module_init);
-mudule_exit(hello_module_cleanup);
+module_exit(hello_module_cleanup);
 
 MODULE_LICENSE("GPL");
