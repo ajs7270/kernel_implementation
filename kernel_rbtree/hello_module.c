9f8b7afd555e9ff4ed78ed686fb211513a67e2c6
jisuAn <ajs7270@naver.com>
2019-09-12 04:26:54 +0900

    fix rbtree

--- kernel_rbtree/hello_module.c
+++ kernel_rbtree/hello_module.c
@@ -4,53 +4,107 @@
 #include <linux/rbtree.h>
 #include <linux/slab.h> // for kmalloc
 
-struct my_node {
+#define FALSE 0
+#define TRUE 1
+
+struct my_type {
 	struct rb_node node;
-	// 100, 300, 200
-	// 100, 108, 116 //sequntialize
-	int key; // rbtree를 정렬하기 위한 기준
-       	int value1;
-	int value2;
-	int value3;
-/*
-           (200,116)
-	   /       \
-    (100,100)     (300,108)
-    /       \     /       \
-    null    null  null   null
-*/
+	int key;	 /* rbtree를 정렬하기 위한 기준 */
+       	int value;
 };
 
-struct my_node *rb_search(struct rb_root *root, int key)
+struct my_type *rb_search(struct rb_root *root, int key)
 {
 	struct rb_node *node = root->rb_node;
 
 	while (node) {
-		struct my_node *data = container_of(node, struct my_node, node)
+		struct my_type *data = container_of(node, struct my_type, node);
+		
+		printk("ing : (key,value)=(%d.%d)\n", data->key,data->value); 
 
+		if(data->key > key)	
+			node = node->rb_left;
+		else if(data->key < key)	
+			node = node->rb_right;
+		else
+			return data;
 	}
-
 	return NULL;
 }
 
+int my_insert(struct rb_root *root, struct my_type *data)
+{
+	struct rb_node **new = &(root->rb_node), *parent = NULL;
+
+	/* Figure out "where" to put new node */
+	while (*new) {
+		struct my_type *this = container_of(*new, struct my_type, node);
+		parent = *new;
+		if (this->key > data->key)
+			new = &((*new)->rb_left);
+		else if (this->key < data->key)
+			new = &((*new)->rb_right);
+		else
+			return FALSE;
+	}
+	
+	/*relinking*/
+	rb_link_node(&data->node, parent, new);
+	/*recoloring & rebalancing*/
+	rb_insert_color(&data->node, root);
+
+	return TRUE;
+
+}
+
+
+int my_delete(struct rb_root *mytree, int key)
+{
+  struct my_type *data = rb_search(mytree, key);
+
+  if (data) {
+        rb_erase(&data->node, mytree);
+        kfree(data);
+  }
+}
+
+
 void struct_exmaple(void)
 {
-	struct my_node *root;
-	struct my_node *hmm;
-	struct rb_node **link = &root.node;
-	struct rb_node *parent = NULL;
-	
 	struct rb_root my_tree = RB_ROOT;
+	int i = 0, ret;
+	for(;i<20;i++){
+		struct my_type *new =kmalloc(sizeof(struct my_type),GFP_KERNEL);
+		if(!new)
+			return NULL;
+				
+		new->value = i*10;
+		new->key = i;
 
-
-	while(*link){
-		parent = *link;
-		entry = rb_entry(parent, struct my_node,node );
-		if(root->data < hmm->data)
-			parent = &(*parent)->rb_left;
-		else if()
-	
+		ret = my_insert(&my_tree,new);
+		printk("JE : return value = %d\n", ret);
 	}
+
+	struct rb_node *node; 
+	for (node = rb_first(&my_tree); node; node = rb_next(node)) 
+        	printk("(key,value)=(%d.%d)\n", rb_entry(node, struct my_type, node)->key,rb_entry(node, struct my_type, node)->value); 
+
+
+	struct my_type *find_node = rb_search(&my_tree,8);
+	if(!find_node){
+		printk("can't find key\n");
+		return ;
+	}	
+	printk("find : (key,value)=(%d.%d)\n", find_node->key,find_node->value); 
+	
+	my_delete(&my_tree,8);
+
+	for (node = rb_first(&my_tree); node; node = rb_next(node)) 
+        	printk("(key,value)=(%d.%d)\n", rb_entry(node, struct my_type, node)->key,rb_entry(node, struct my_type, node)->value); 
+
+
+	
+	
 }
 
 int __init hello_module_init(void)
